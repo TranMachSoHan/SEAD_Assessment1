d89ec6a050748c7ac819fec9ead71a0fd9600021
TranMachSoHan <50974854+TranMachSoHan@users.noreply.github.com>
2022-12-02 16:59:41 +0700

    ADD home page

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import './App.css';
 import HotelDetail from './pages/HotelDetail/HotelDetail';
 import Admin from './pages/Admin/Admin';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import Homepage from './pages/Admin/Homepage';
-import HotelRoomList from './pages/Admin/HotelRoomList';
+import Home from './pages/Home/Home';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
+          <Route path='/' element={<Home/>} />
           <Route path='/visitor' element={<HotelDetail/>}/>
           <Route path='/admin/*' element={<Admin/>}/>
         </Routes>
